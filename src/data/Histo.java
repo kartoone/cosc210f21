77723a68324612c8bd1b8c49c9f16ab98e67cce8
kartoone <brtoone@samford.edu>
2021-09-12 08:28:50 -0500

    added comments

--- src/data/Histo.java
+++ src/data/Histo.java
@@ -5,12 +5,25 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+// Histo - short for histogram 
 public class Histo {
 
+	// remember, all java programs start with a main() method inside a class
+	// also, the FileNotFoundException must either be handled or thrown, it's less code if we just "throw" it, but it makes our program less robust because it doesn't handle the error where the specified file is not found
 	public static void main(String[] args) throws FileNotFoundException {
+
+		// create a Scanner object named filein that we can use to read through a file
 		Scanner filein = new Scanner(new File("test.txt"));
+		
+		// create a native java array named counts[] because we know exactly how many elements we need and what to initialize them to
 		int counts[] = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+		// we DON'T know how many numbers are going to be in the file, so it makes sense to use
+		// an ArrayList instead of a native java array. The ArrayList named allTheDigits will grow
+		// dynamically as we add items to it
 		ArrayList<Integer> allTheDigits = new ArrayList<>();
+		
+		// note the while loop here ... remember that scanners almost always use while loops
 		while (filein.hasNextLine()) {
 			int digit = filein.nextInt();
 			allTheDigits.add(digit);
@@ -19,6 +32,10 @@ public class Histo {
 		for (int i = 0; i < counts.length; i++) {
 			System.out.println(i + " occurred " + counts[i] + " times.");
 		}
+		
+		// note that ArrayList has a very handy "toString()" method that gets called automatically
+		// by the println method and displays a nicely formatted list of all its data without us
+		// having to use a for loop
 		System.out.println(allTheDigits);
 		
 	}
