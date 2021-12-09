bfee62fd73eed460c3f412210427ad4d3df65747
Brian Toone <brtoone@samford.edu>
2021-12-09 11:20:13 -0600

    puzzle2 solved for day 9

--- src/acm/AdventDay9.java
+++ src/acm/AdventDay9.java
@@ -108,167 +108,75 @@ public class AdventDay9 {
 			}
 		}
 		
+		// answer to puzzle 2 is the product of the SIZE of the three largest basins ... so sort by basin size 
 		Collections.sort(lowpoints);
 		
-		int lptotal = 0;
-		for (Lowpoint lp : lowpoints) {
-			System.out.println(lp);
-			lptotal = lptotal + lp.lpdata[2] + 1;
-		}
-		System.out.println(lptotal);		
+//		for (Lowpoint lp : lowpoints) {
+//			System.out.println(lp);
+//		}
+		
+		// print out the product of the sizes
 		System.out.println(lowpoints.get(0).lpdata[3]*lowpoints.get(1).lpdata[3]*lowpoints.get(2).lpdata[3]); 
 	}
 
-	// returns the size of the basin b/c that's all we need
+	// recursive approach ... the key to solving this one is doing it recursively AND checking that you haven't already included a given coordinate as part of the basin
+	//   my first thought was to try the "walking" approach but we don't need to do that b/c we are already INSIDE the basin
+	//   since we are already inside, we can just recursively check the neighbors and add them to the basin if the aren't already there
+	//   avoid infinite recursion by making sure they aren't already there. when a given branch hits a dead end, it simply stops
+	//   meanwhile there are a bunch of other recursive branches that are going to end up finding all the members of the basin!
 	private static int findBasin(int r, int c, int[][] grid) {
 		// first ... expand the grid and surround it completely with 9s so that we don't have to worry about going out of bounds
-		int newgrid[][][] = new int[grid.length+2][grid[0].length+2][1];
+		int newgrid[][] = new int[grid.length+2][grid[0].length+2];
 		for (int i=0; i<newgrid.length; i++) {
 			for (int j=0; j<newgrid[0].length; j++) {
 				if (i==0 || j==0 || i==newgrid.length-1 || j==newgrid[0].length-1)
-					newgrid[i][j] = new int[] {9, 0};
+					newgrid[i][j] = 9;
 				else
-					newgrid[i][j] = new int[] {grid[i-1][j-1], 0};
+					newgrid[i][j] = grid[i-1][j-1];
 			}
 		}		
-		// start from basin and head north until we hit a wall and then start applying right-hand rule to completely traverse wall
-		// counter-clockwise with right-hand always on wall facing: 'n', 'w', 's', 'e'
-		// if we get stuck jump back to beginning and repeat but this time go clockwise using the lefthand rule
-		// if we get stuck again, jump back to beginning and repeat process (if not stuck)
-		
 		ArrayList<int[]> basinCoords = new ArrayList<>();
 		basinCoords.add(new int[] {r,c});
-		char facing = 'n';
-		int locr = r+1;
-		int locc = c+1;
-		int state = 0; 		// the idea behind state is ... 
-							// 0 means get as far north as possible
-							// 1 means we have started to move counterclockwise tracing the edges of the wall ... allowed to backtrack over spaces behind us if necessary ... but if run over first spot where we hit wall switch to state 2
-							// 2 means we are still counterclockwise tracing, but this time if we hit the original spot again we increment our "wallcounter" and are no longer allowed to step on walls with a counter<wallcounter but go ahead and switch back to state 1
-		int nwr = -1;
-		int nwc = -1;
-		int wallcount = 1;
-		printNearbygrid(10, state, wallcount, r+1, c+1, newgrid);
-		while (true) { 
-			// we will break out of the loop at necessary point ... so we have to label the loop since we are also using a switch stmt ... could avoid label by using if ... else if ...
-			if (state==0) { // the "go north" state
-				// if we are in state 0 and back at the beginning where we started ... we have marked everything
-				if (completelySurrounded(wallcount, locr, locc, newgrid))
-					break; // this breaks out ... supposedly we should have marked everything by now
-				
-				if (!isItReallyWall(wallcount, locr-1, locc, newgrid)) {
-					locr--;
-				} else {
-					// we hit the north wall! ... save the spot where we hit and transition to state 1
-					nwr=locr;
-					nwc=locc;
-					state = 1;
-					facing = 'w'; // saves a step
-				}
-			} else if (state==2) { // the "go south" state
-					// if we are in state 0 and back at the beginning where we started ... we have marked everything
-					if (completelySurrounded(wallcount, locr, locc, newgrid))
-						break; // this breaks out ... supposedly we should have marked everything by now
-					
-					if (!isItReallyWall(wallcount, locr+1, locc, newgrid)) {
-						locr++;
-					} else {
-						// we hit the north wall! ... save the spot where we hit and transition to state 1
-						nwr=locr;
-						nwc=locc;
-						state = 1;
-						facing = 'e'; // saves a step
-					}
-			} else if (state==1) { // the "counterclockwise" "right-hand rule" traversal
-				switch(facing) {
-					case 'w':
-						// facing west ... only move west if there is not a wall there AND if there is still a wall to our right (north)
-						if (!isItReallyWall(wallcount, locr, locc-1, newgrid) && isItReallyWall(wallcount, locr-1, locc, newgrid)) {
-							// go west!
-							locc--;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-						} else if (!isItReallyWall(wallcount, locr-1, locc, newgrid)) {
-							// move north if we find an empty spot there
-							locr--;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-							facing='n';
-						} else {
-							// we only make it here if we can't go west ... so let's turn south
-							facing='s';
-						}
-						break;
-					case 'n':
-						// facing north ... only move north if there is not a wall there AND if there is still a wall to our right (east)
-						if (!isItReallyWall(wallcount, locr-1, locc, newgrid) && isItReallyWall(wallcount, locr, locc+1, newgrid)) {
-							// go north!
-							locr--;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-						} else if (!isItReallyWall(wallcount, locr, locc+1, newgrid)) {
-							// move east if we find an empty spot there
-							locc++;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-							facing='e';
-						} else {
-							// we only make it here if we can't go east ... so let's turn back west
-							facing='w';
-						}
-						break;
-					case 'e':
-						// facing east ... only move east if there is not a wall there AND if there is still a wall to our right (south)
-						if (!isItReallyWall(wallcount, locr, locc+1, newgrid) && isItReallyWall(wallcount, locr+1, locc, newgrid)) {
-							// go east!
-							locc++;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-						} else if (!isItReallyWall(wallcount, locr+1, locc, newgrid)) {
-							// move south if we find an empty spot there
-							locr++;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-							facing='s';
-						} else {
-							// we only make it here if we can't go south ... so let's turn back north
-							facing='n';
-						}
-						break;
-					case 's':
-						// facing south ... only move south if there is not a wall there AND if there is still a wall to our right (west)
-						if (!isItReallyWall(wallcount, locr+1, locc, newgrid) && isItReallyWall(wallcount, locr, locc-1, newgrid)) {
-							// go south!
-							locr++;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-						} else if (!isItReallyWall(wallcount, locr, locc-1, newgrid)) {
-							// move west if we find an empty spot there
-							locc--;
-							addCoord(wallcount, locr, locc, newgrid, basinCoords);
-							facing='w';
-						} else {
-							// we only make it here if we can't go south ... so let's turn back east
-							facing='e';
-						}
-						break;
-				}
-				// see if we are back at where we first hit the wall
-				if (locr == nwr && locc==nwc) {
-					wallcount++;
-					state = 2;
-				}
-				
-			} else {
-			}
-			printNearbygrid(10, state, wallcount, r+1, c+1, newgrid);
-		}
-		System.exit(1);
-		
-		// since we have list of visited coords and can "jump" ... use this strategy
-		// 
+//		printNearbygrid(10, r+1, c+1, newgrid);
+		fbHelper(r+1, c+1, newgrid, basinCoords);
+//		System.out.println(basinCoords.size());
 		return basinCoords.size();
 	}
 	
-	private static void printNearbygrid(int sq, int state, int wallcount, int r, int c, int[][][]grid) {
-		System.out.println(r + " " + c + " " + state + " " + wallcount);
-		for (int i=r-20; i<r+sq; i++) {
-			for (int j=c-20; j<c+sq; j++) {
+	private static void fbHelper(int r, int c, int[][] grid, ArrayList<int[]> basinCoords) {
+		// try north first
+		if (grid[r-1][c]!=9 && !containsArray(basinCoords, new int[] {r-1,c})) {
+			basinCoords.add(new int[] {r-1, c});
+			fbHelper(r-1, c, grid, basinCoords);
+		} 
+		// try west next
+		if (grid[r][c-1]!=9 && !containsArray(basinCoords, new int[] {r,c-1})) {
+			basinCoords.add(new int[] {r, c-1});
+			fbHelper(r, c-1, grid, basinCoords);
+		}  
+		// try east next
+		if (grid[r][c+1]!=9 && !containsArray(basinCoords, new int[] {r,c+1})) {
+			basinCoords.add(new int[] {r, c+1});
+			fbHelper(r, c+1, grid, basinCoords);
+		}  
+		// try south last 
+		if (grid[r+1][c]!=9 && !containsArray(basinCoords, new int[] {r+1,c})) {
+			basinCoords.add(new int[] {r+1, c});
+			fbHelper(r+1, c, grid, basinCoords);
+		}  
+	}
+	
+	private static boolean containsArray(ArrayList<int[]> list, int[] coords) {
+	    return list.stream().anyMatch(a -> java.util.Arrays.equals(a, coords));
+	}
+
+	
+	private static void printNearbygrid(int sq, int r, int c, int[][]grid) {
+		System.out.println(r + " " + c + " " );
+		for (int i=r-sq; i<r+sq; i++) {
+			for (int j=c-sq; j<c+sq; j++) {
 				if (i>=0 && j>=0 && i<grid.length && j<grid.length)
-					System.out.print(grid[i][j][0]);
+					System.out.print(grid[i][j]);
 			}
 			if (i>=0 && i<grid.length)
 				System.out.println();
@@ -276,32 +184,6 @@ public class AdventDay9 {
 		System.out.println();
 	}
 
-	private static void addCoord(int wallcount, int r, int c, int[][][] newgrid, ArrayList<int[]> basinCoords) {
-		newgrid[r][c] = new int[] {9, wallcount};
-		boolean alreadythere = false;
-		for (int[] coord: basinCoords) {
-			if (coord[0]==r && coord[1]==c) {
-				alreadythere = true;
-				break;
-			}
-		}
-		if (!alreadythere) {
-			basinCoords.add(new int[] {r, c});
-		}
-	}
-
-	private static boolean completelySurrounded(int wallcount, int locr, int locc, int[][][] newgrid) {
-		return isItReallyWall(wallcount, locr-1, locc, newgrid)&&
-				isItReallyWall(wallcount, locr, locc-1, newgrid)&&
-				isItReallyWall(wallcount, locr+1, locc, newgrid)&&
-				isItReallyWall(wallcount, locr, locc+1, newgrid);
-	}
-
-	// marking the "walls" in phases ... so that you can backtrack over a wall once, but not twice
-	private static boolean isItReallyWall(int wc, int r, int c, int[][][] newgrid) {
-		return newgrid[r][c][0]==9 && newgrid[r][c][1]<wc;
-	}
-	
 	private static boolean isLower(int depth, int r, int c, int[][] grid) {
 		if (r>=0 && r<grid.length && c>=0 && c<grid[0].length)
 			return depth<grid[r][c];
